fff364fbb4fd3c43dc450c57bdacad5860755503
ifilipechau <filipechau.dev@gmail.com>
2025-07-08 15:20:43 +0200

    feat: implement Dark Mode and Light Mode features

--- js/script.js
+++ js/script.js
@@ -13,3 +13,17 @@ menu.onclick = () => {
 window.onscroll = () => {
     navbar.classList.remove("active");
 };
+
+// Dark Mode and Light Mode
+
+let darkmode = document.querySelector("#darkmode");
+
+darkmode.onclick = () => {
+    if(darkmode.classList.contains("bx-sun")) {
+        darkmode.classList.replace("bx-sun", "bx-moon");
+        document.body.classList.add("active");
+    } else {
+        darkmode.classList.replace("bx-moon", "bx-sun");
+        document.body.classList.remove("active");
+    }
+};
