becb7084ee15caf6cefd7a6d8578344dabe1b77d
ifilipechau <filipechau.dev@gmail.com>
2025-07-08 15:22:21 +0200

    feat: implement swiper library for slides including navigation arrows

--- js/script.js
+++ js/script.js
@@ -27,3 +27,24 @@ darkmode.onclick = () => {
         document.body.classList.remove("active");
     }
 };
+
+// swiper library for slides 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    },
+    autoplay: {
+        delay: 2000,
+    },
+
+    // Navigation arrows
+    navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    },
+});
