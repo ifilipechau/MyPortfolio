3d28fb0fd077334541043da6e7d2417deae05b6f
ifilipechau <filipechau.dev@gmail.com>
2025-07-08 15:19:24 +0200

    feat: implement a toggle navbar

--- js/script.js
+++ js/script.js
@@ -6,3 +6,10 @@ let navbar = document.querySelector(".navbar");
 window.addEventListener("scroll", () => {
     header.classList.toggle("shadow", window.scrollY > 0);
 });
+
+menu.onclick = () => {
+    navbar.classList.toggle("active");
+};
+window.onscroll = () => {
+    navbar.classList.remove("active");
+};
